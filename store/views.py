1dce60da114427382b5282270e4ccdff49bab681
Ara Darbinyan <aradarbinyan.2003@gmail.com>
2025-07-08 22:20:44 +0400

    created about and contact, upgraded base and index, added photoes

--- store/views.py
+++ store/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'store/index.html')
+
+
+def about(request):
+    return render(request, 'store/about.html')
+
+
+def contact(request):
+    return render(request, 'store/contact.html')
