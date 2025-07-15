f40530dd1d274e1eff73c39d4efea95df5ebefdb
Ara Darbinyan <aradarbinyan.2003@gmail.com>
2025-07-15 19:10:43 +0400

    added cart where a users can see his products

--- store/views.py
+++ store/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegisterForm
-from .models import Book
+from .models import Book, Cart, CartItem
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def index(request):
@@ -19,6 +20,47 @@ def all_books_view(request):
 def book_detail_view(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'store/book_detail.html', {'book': book})
+
+
+@login_required
+def cart_view(request):
+    customer = request.user.customer
+    cart = Cart.objects.filter(customer=customer, is_active=True).first()
+
+    if cart:
+        items = CartItem.objects.filter(cart=cart)
+        total_price = sum(item.get_total_price() for item in items)
+    else:
+        items = []
+        total_price = 0
+
+    return render(request, 'store/cart.html', {
+        'cart': cart,
+        'items': items,
+        'total_price': total_price
+    })
+
+
+@login_required
+@require_POST
+def add_to_cart(request, book_id):
+    customer = request.user.customer
+    cart, created = Cart.objects.get_or_create(customer=customer, is_active=True)
+    book = get_object_or_404(Book, id=book_id)
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, book=book)
+
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    return redirect('cart')
+    
+
+@login_required
+def remove_from_cart(request, item_id):
+    item = get_object_or_404(CartItem, id=item_id)
+    item.delete()
+    return redirect('cart')
 
 
 def about(request):
